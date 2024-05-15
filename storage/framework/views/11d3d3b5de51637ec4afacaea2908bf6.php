602ee09b8e8af7af78ca9eead47ff4a1463e7512
furlax <furlax@proton.me>
2024-05-15 08:14:15 -0700

    filesssss

---
+++ storage/framework/views/11d3d3b5de51637ec4afacaea2908bf6.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Luca\Desktop\School\Semestre 6\Architecture\DataFighterAPI\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
